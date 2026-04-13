277f5b4fa90c8464343a67a4ea5028f373251f89
canis <math.caceres@duocuc.cl>
2026-04-13 10:04:47 -0400

    variable nuev

--- hhhh.py
+++ hhhh.py
@@ -1,1 +1,18 @@
 print ("hola mathias")
+
+# creando variables
+
+titulo="clima de hoy"  # string
+DiaDelMes=13  # int
+Mes=4  #int
+
+temperatura=22.3 # float
+
+llueve=False # boolean
+
+print(titulo)
+print("temperatura actual", temperatura, "grados")
+print(DiaDelMes, "-", Mes)
+
+# "rojo"== 
+#
